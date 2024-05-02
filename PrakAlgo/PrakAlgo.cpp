523bcf359da538c30641c7642ddab30cec68e2a2
dutabrahmana <dutalinggau25@gmail.com>
2024-05-02 11:03:29 +0700

    Add project files.

---
+++ PrakAlgo/PrakAlgo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+struct Node {
+	int noMhs;
+	string namae;
+	Node* next;
+	Node* prev;
+};
+
+Node* START = NULL;
+
+void addNode() {
+
+}
+
