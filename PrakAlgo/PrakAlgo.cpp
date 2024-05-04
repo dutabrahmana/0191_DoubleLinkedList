5a78ec5bfc7328207a2b588e5de837f1dd0cb0fd
dutabrahmana <dutalinggau25@gmail.com>
2024-05-04 10:08:43 +0700

    memnangil input dan output

--- PrakAlgo/PrakAlgo.cpp
+++ PrakAlgo/PrakAlgo.cpp
@@ -77,6 +77,8 @@ void deleteNode()
 {
     Node* previous, * current;
     int rollNo;
-                                                               //step 3get the roll number to be deleted
+
+    cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+    cin >> rollNo;                                                                  //step 3get the roll number to be deleted
 
    
